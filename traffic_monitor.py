53e1916aef4eea1e7aacbfb3780bcb23e0f4cc3b
Bernardo Farinon <bernardo.farinon@edu.pucrs.br>
2025-11-17 18:12:44 -0300

    add initial functions

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -1,1 +1,14 @@
 # socket raw to monitor network traffic - captures packets on the network interface and process to indentify traffic types and log them 
+
+import socket
+
+#def monitor_traffic():
+
+#    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        
+#def process_packet(packet):
+
+# def log_traffic_info(info):
+
+# if __name__ == "__main__":
+#    monitor_traffic()
